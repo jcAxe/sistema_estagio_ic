b4c87ae88371444dc118c40d1d6c3bb785b66728
joseh <jcamachado@id.uff.br>
2018-07-08 12:30:51 -0300

    feat: create jobs application and its basics templates and references to these templates

---
+++ estagio_ic/jobs/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+from jobs import views
+
+urlpatterns = [
+
+    url(r'^list_job_opportunity/$', views.list_job_opportunity, name='list_job_opportunity'),
+    url(r'^display_job_opportunity/$', views.display_job_opportunity, name='display_job_opportunity'),
+    url(r'^list_job_application/$', views.list_job_application, name='list_job_application'),
+    url(r'^display_job_application/$', views.display_job_application, name='display_job_application'),
+]
---
+++ estagio_ic/jobs/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+
+def list_job_opportunity(request):
+    return render(request, 'jobs/list_job_opportunity.html')
+
+
+def display_job_opportunity(request):
+    return render(request, 'jobs/display_job_opportunity.html')
+
+
+def list_job_application(request):
+    return render(request, 'jobs/list_job_application.html')
+
+
+def display_job_application(request):
+    return render(request, 'jobs/display_job_application.html')
